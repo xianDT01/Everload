80bf36140e5a68e712b37d6beb2c874af526d69f
xianDT <xiandt01@gmail.com>
2025-03-03 21:03:48 +0100

    Swagger edded

---
+++ src/main/java/com/EverLoad/everload/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.EverLoad.everload.config;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("XianBlogSpot API")
+                        .description("Documentación de la API de XianBlogSpot")
+                        .version("v1.0"))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Proyecto GitHub")
+                        .url("https://github.com/xianDT01/XianBlogSpot"));
+    }
+}
