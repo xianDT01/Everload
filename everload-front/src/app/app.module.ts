5c0fb5c17243c091ca2fe558c25f6b5d92569483
xianDT <xiandt01@gmail.com>
2025-03-14 22:59:18 +0100

    YouTube downloads added locally

--- everload-front/src/app/app.module.ts
+++ everload-front/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
@@ -16,7 +17,8 @@ import { YoutubeDownloadsComponent } from './components/youtube-downloads/youtub
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
