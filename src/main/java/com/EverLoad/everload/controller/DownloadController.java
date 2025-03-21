61621d229470e34e77c23470ba72948991f583e2
xianDT <xiandt01@gmail.com>
2025-03-21 13:32:32 +0100

    youtubeController added

--- src/main/java/com/EverLoad/everload/controller/DownloadController.java
+++ src/main/java/com/EverLoad/everload/controller/DownloadController.java
@@ -34,27 +34,4 @@ public class DownloadController {
             @RequestParam String format) {
         return downloadService.downloadMusic(videoId, format);
     }
-
-
-    @RestController
-    @RequestMapping("/api/youtube")
-    @CrossOrigin(origins = "http://localhost:4200")
-    public class YouTubeController {
-
-        private final RestTemplate restTemplate;
-        private final String API_KEY = "AIzaSyCVzVmbSB5YVeYzOfiUtw3Hx_J58nGytxI";
-
-        public YouTubeController(RestTemplate restTemplate) {
-            this.restTemplate = restTemplate;
-        }
-
-        @GetMapping("/search")
-        public ResponseEntity<String> searchVideos(@RequestParam String query) {
-            String url = "https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=10&q="
-                    + query + "&key=" + API_KEY;
-
-            String response = restTemplate.getForObject(url, String.class);
-            return ResponseEntity.ok(response);
-        }
-    }
 }
