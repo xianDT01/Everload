45b7e6e24c417009f112656c02f3764a2373836d
xianDT <xiandt01@gmail.com>
2025-03-26 13:11:54 +0100

    Download on Facebook, Instagram added

--- everload-front/src/app/app-routing.module.ts
+++ everload-front/src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { YoutubeDownloadsComponent } from './components/youtube-downloads/youtube-downloads.component';
 import { TwitterDownloadsComponent } from './components/twitter-downloads/twitter-downloads.component'; 
-
+import { FacebookDownloadsComponent } from './components/facebook-downloads/facebook-downloads.component';
+import { InstagramDownloadsComponent } from './components/instagram-downloads/instagram-downloads.component';
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'youtube-downloads', component: YoutubeDownloadsComponent },
-  { path: 'twitter-downloads', component: TwitterDownloadsComponent } 
+  { path: 'twitter-downloads', component: TwitterDownloadsComponent },
+  {path: 'facebook-downloads', component: FacebookDownloadsComponent},
+  {path : 'instagram-downloads', component: InstagramDownloadsComponent} 
 ];
 
 @NgModule({
