92f904ac0a492f5f9fd95e90a3c3341e22b08f9f
xianDT <xiandt01@gmail.com>
2025-05-01 22:27:13 +0200

    added test youtubeSearchShouldReturnNonEmptyBody

--- src/test/java/com/EverLoad/everload/EverloadApplicationTests.java
+++ src/test/java/com/EverLoad/everload/EverloadApplicationTests.java
@@ -6,8 +6,10 @@
 	import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 	import org.springframework.boot.test.context.SpringBootTest;
 	import org.springframework.test.web.servlet.MockMvc;
+	import org.springframework.test.web.servlet.ResultActions;
 
 	import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+	import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 	import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 	@SpringBootTest
@@ -49,6 +51,14 @@
 		void searchYouTubeWithoutQueryShouldFail() throws Exception {
 			mockMvc.perform(get("/api/youtube/search"))
 					.andExpect(status().isBadRequest());
+		}
+
+		@Test
+		void youtubeSearchShouldReturnNonEmptyBody() throws Exception {
+			ResultActions result = mockMvc.perform(get("/api/youtube/search")
+							.param("query", "música"))
+					.andExpect(status().isOk())
+					.andExpect(content().string(org.hamcrest.Matchers.containsString("items")));
 		}
 
 	}
