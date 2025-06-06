f777e23211c6a16e7ae73ec0bd0276318d9068c3
xianDT <xiandt01@gmail.com>
2025-06-06 11:53:30 +0200

    updated responsive layout to display Facebook and Instagram logos on mobile and remove dark background

--- everload-front/src/app/components/youtube-downloads/youtube-downloads.component.ts
+++ everload-front/src/app/components/youtube-downloads/youtube-downloads.component.ts
@@ -12,8 +12,8 @@ export class YoutubeDownloadsComponent {
   videoUrl: string = '';
   resolution: string = '720';
   isLoading: boolean = false;
-//  backendUrl: string = 'http://localhost:8080/api';
- backendUrl: string = '/api';
+  backendUrl: string = 'http://localhost:8080/api';
+// backendUrl: string = '/api';
 
   searchResults: any[] = [];
   searchQuery: string = '';
