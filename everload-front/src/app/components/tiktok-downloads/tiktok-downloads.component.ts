73ca595079b208f2eedf0830fbba9af841c2dbc6
xianDT <xiandt01@gmail.com>
2025-07-28 12:49:33 +0200

    apiUrl added to host

--- everload-front/src/app/components/tiktok-downloads/tiktok-downloads.component.ts
+++ everload-front/src/app/components/tiktok-downloads/tiktok-downloads.component.ts
@@ -34,6 +34,7 @@ export class TiktokDownloadsComponent {
     this.error = null;
 
     const apiUrl = 'http://localhost:8080/api/downloadTikTok';
+  //  const apiUrl = '/api/downloadTikTok';
     const params = { url: this.tiktokUrl };
 
     this.http.get(apiUrl, {
