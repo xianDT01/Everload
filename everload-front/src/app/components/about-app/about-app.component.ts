ac2f08895dee7fa969991a810d16e650eb0b1374
xianDT <xiandt01@gmail.com>
2025-08-25 17:57:57 +0200

    icons of the platforms on the about page added

--- everload-front/src/app/components/about-app/about-app.component.ts
+++ everload-front/src/app/components/about-app/about-app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import { gsap } from 'gsap';
+import { MorphSVGPlugin } from 'gsap/MorphSVGPlugin';
+
+gsap.registerPlugin(MorphSVGPlugin);
 
 @Component({
   selector: 'app-about-app',
   templateUrl: './about-app.component.html',
   styleUrls: ['./about-app.component.css']
 })
-export class AboutAppComponent {
+export class AboutAppComponent implements AfterViewInit {
 
+  ngAfterViewInit(): void {
+    document.getElementById("fbIcon")?.addEventListener("click", () => {
+      gsap.to("#fbLetter", { duration: 1, morphSVG: "#like" });
+    });
+
+    document.getElementById("igIcon")?.addEventListener("click", () => {
+      gsap.to(".cameraSquareTwo", { duration: 1, morphSVG: "#heart" });
+      document.getElementById("camera")!.style.display = "none";
+      document.getElementById("dot")!.style.display = "none";
+    });
+
+    document.getElementById("twitterIcon")?.addEventListener("click", () => {
+      gsap.to("#bird", { duration: 1, morphSVG: ".addIcon" });
+    });
+
+    document.getElementById("youtubeIcon")?.addEventListener("click", () => {
+      gsap.to(".play", { duration: 1, morphSVG: "#click" });
+      document.getElementById("extra")!.style.display = "none";
+    });
+  }
 }
