7437ce71f1ec8e899d30e7f7e2c9ad32ece37a5d
xianDT <xiandt01@gmail.com>
2025-09-26 17:55:55 +0200

    constructor_initializesFieldsCorrectly

--- src/test/java/com/EverLoad/everload/SpotifyResultTest.java
+++ src/test/java/com/EverLoad/everload/SpotifyResultTest.java
@@ -3,6 +3,7 @@ package com.EverLoad.everload;
 import com.EverLoad.everload.model.SpotifyResult;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SpotifyResultTest {
@@ -16,4 +17,12 @@ public class SpotifyResultTest {
         assertEquals(title, result.getTitle());
         assertEquals(youtubeUrl, result.getYoutubeUrl());
     }
+    @Test
+    void constructor_asignaValoresCorrectamente() {
+        SpotifyResult result = new SpotifyResult("Song A - Artist", "https://youtube.com/watch?v=12345");
+
+        assertThat(result.getTitle()).isEqualTo("Song A - Artist");
+        assertThat(result.getYoutubeUrl()).isEqualTo("https://youtube.com/watch?v=12345");
+    }
+
 }
